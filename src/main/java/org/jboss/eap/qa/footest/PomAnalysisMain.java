0f894ab4013bd08a7c7fcad436d2fee1eccaba32
Rostislav Svoboda <rsvoboda@redhat.com>
2019-02-01 13:15:49 +0100

    Analysis of properties in pom.xml file - check against online master

--- src/main/java/org/jboss/eap/qa/footest/PomAnalysisMain.java
+++ src/main/java/org/jboss/eap/qa/footest/PomAnalysisMain.java
@@ -5,36 +5,62 @@ import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Map;
 import java.util.Properties;
 
 public class PomAnalysisMain {
 
+    private static final String FILE_URL = "https://raw.githubusercontent.com/wildfly/wildfly/master/pom.xml";
+
     public static void main(String... args) {
         try {
             Model newModel = pomToModel("/Users/rsvoboda/git/wildfly/pom.xml");
-            Model oldModel = pomToModel("/Users/rsvoboda/tmp/wildfly/pom.xml");
+//            Model oldModel = pomToModel("/Users/rsvoboda/tmp/wildfly/pom.xml");
+
+            Path tempFile = Files.createTempFile("PomAnalysis-wildfly-", ".tmp");
+            InputStream in = new URL(FILE_URL).openStream();
+            Files.copy(in, tempFile, StandardCopyOption.REPLACE_EXISTING);
+            Model oldModel = pomToModel(tempFile.toString());
+            Files.delete(tempFile);
 
             Properties newProperties = newModel.getProperties();
             Properties oldProperties = oldModel.getProperties();
+            StringBuilder sb = new StringBuilder();
 
             // removed / added properties
             for (String key : newProperties.stringPropertyNames()) {
                 if (! oldProperties.containsKey(key)) {
-                    System.out.println("Added property: " + key);
+                    sb.append("Added property: ");
+                    sb.append(key);
+                    sb.append(System.getProperty("line.separator"));
                 }
             }
             for (String key : oldProperties.stringPropertyNames()) {
                 if (! newProperties.containsKey(key)) {
-                    System.out.println("Removed property: " + key);
+                    sb.append("Removed property: ");
+                    sb.append(key);
+                    sb.append(System.getProperty("line.separator"));
                 }
             }
             // changed properties
             for (Map.Entry<Object, Object> entry : newProperties.entrySet()) {
                 Object oldValue = oldProperties.get(entry.getKey());
                 if (oldValue != null && !entry.getValue().equals(oldValue)) {
-                    System.out.printf("Changed property: %s from %s to %s\n", entry.getKey(), oldValue, entry.getValue());
+                    sb.append("Changed property: ");
+                    sb.append(String.format("%s from %s to %s\n", entry.getKey(), oldValue, entry.getValue()));
+                    sb.append(System.getProperty("line.separator"));
                 }
+            }
+
+            if (sb.length() > 0) {
+                System.out.println(sb.toString());
+            } else {
+                System.out.println("No changes");
             }
         } catch (Exception e) {
             e.printStackTrace();
